a17725fb370b159b04929f53ee1fce438f151d4b
Abdullah-142 <abdulah14200@gmail.com>
2024-03-25 08:28:41 -0700

    Refactor Home component to use useState and useEffect hooks

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-import Image from "next/image";
+"use client";
 
+import { useEffect, useState } from "react";
 export default function Home() {
-   return <div>
-      
-   </div>;
+   const [state, setState] = useState(0);
+
+   useEffect(() => {
+      const sum = () => {
+         let a = 10;
+         let b = 20;
+         let c = a + b;
+         setState(c);
+      };
+
+      sum();
+   }, [state]);
+   return (
+      <>
+         <h1 className="text-4xl grid h-screen place-content-center bg-pink-700">
+            {state}
+         </h1>
+      </>
+   );
 }
