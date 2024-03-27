641c4e1d49b21e61c78e67bab5a518cc5109259a
Abdullah-142 <abdulah14200@gmail.com>
2024-03-27 09:00:47 -0700

    Add mongoose package and fix import in Header component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,10 +2,8 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
-import NavItems from "./NavItems";
 import MobileNav from "./MobileNav";
-// import NavItems from "./NavItems";
-// import MobileNav from "./MobileNav";
+import NavItems from "./NavItems";
 
 const Header = () => {
    return (
---
+++ lib/database/index.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+let cached = (global as any).mongoose || {
+   con: null,
+   promise: null,
+};
+
+export const connectToDatabase = async () => {
+   if (cached.con) {
+      return cached.con;
+   }
+
+   if (!cached.promise) {
+      if (!process.env.MONGODB_URI) {
+         throw new Error(
+            "Please define the MONGODB_URI environment variable inside .env.local"
+         );
+      }
+      cached.promise =
+         cached.promise ||
+         mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "Effortless Events",
+            bufferCommands: false,
+         });
+   }
+
+   cached.con = await cached.promise;
+
+   return cached.con;
+};
