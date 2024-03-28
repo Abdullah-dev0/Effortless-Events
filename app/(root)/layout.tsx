3b747549ef512a8f855cfa91f21a91c86c924de0
Abdullah-142 <abdulah14200@gmail.com>
2024-03-27 19:35:13 -0700

    Update import path for Footer component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import { Footer } from "../../components/shared/Footer";
+import { Footer } from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
 
 export default function RootLayout({
