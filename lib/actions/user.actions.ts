162ee462634394f346d47cafaba3d6601fe48ccf
Abdullah-142 <abdulah14200@gmail.com>
2024-03-27 19:15:23 -0700

    Refactor user actions and add getUserById function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,11 @@
 "use server";
 
-import { CreateUserParams } from "@/types";
 import User from "@/lib/database/models/user.model";
+import { CreateUserParams } from "@/types";
+import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../database";
+import Event from "../database/models/event.model";
+import Order from "../database/models/order.model";
 import { handleError } from "../utils";
 
 const createUser = async (user: CreateUserParams) => {
@@ -16,23 +19,35 @@ const createUser = async (user: CreateUserParams) => {
    }
 };
 
+const getUserByid = async (clerkId: string) => {
+   try {
+      await connectToDatabase();
+
+      const user = await User.findById(clerkId);
+
+      return JSON.parse(JSON.stringify(user));
+   } catch (error) {
+      handleError(error);
+   }
+};
+
 const updateUser = async (clerkId: string, user: CreateUserParams) => {
    try {
       await connectToDatabase();
-    const updatedUser = await User.findOneAndUpdate(
-       { clerkId },
-       {
-          $set: {
-             clerkId: user.clerkId,
-             email: user.email,
-             username: user.username,
-             firstName: user.firstName,
-             lastName: user.lastName,
-             photo: user.photo,
-          },
-       },
-       { new: true }
-    );
+      const updatedUser = await User.findOneAndUpdate(
+         { clerkId },
+         {
+            $set: {
+               clerkId: user.clerkId,
+               email: user.email,
+               username: user.username,
+               firstName: user.firstName,
+               lastName: user.lastName,
+               photo: user.photo,
+            },
+         },
+         { new: true }
+      );
 
       return JSON.parse(JSON.stringify(updatedUser));
    } catch (error) {
@@ -43,12 +58,35 @@ const updateUser = async (clerkId: string, user: CreateUserParams) => {
 const deleteUser = async (clerkId: string) => {
    try {
       await connectToDatabase();
-      const deletedUser = await User.findOneAndDelete({ clerkId });
 
-      return JSON.parse(JSON.stringify(deletedUser));
+      // Find user to delete
+      const userToDelete = await User.findOne({ clerkId });
+
+      if (!userToDelete) {
+         throw new Error("User not found");
+      }
+
+      // Unlink relationships
+      await Promise.all([
+         // Update the 'events' collection to remove references to the user
+         Event.updateMany(
+            { _id: { $in: userToDelete.events } },
+            { $pull: { organizer: userToDelete._id } }
+         ),
+
+         // Update the 'orders' collection to remove references to the user
+         Order.updateMany(
+            { _id: { $in: userToDelete.orders } },
+            { $unset: { buyer: 1 } }
+         ),
+      ]);
+
+      // Delete user
+      const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+      revalidatePath("/");
    } catch (error) {
       handleError(error);
    }
 };
 
-export { createUser, updateUser, deleteUser };
+export { createUser, deleteUser, getUserByid, updateUser };
---
+++ lib/database/models/order.model.ts
@@ -0,0 +1,51 @@
+import { Schema, model, models, Document } from "mongoose";
+
+export interface IOrder extends Document {
+   createdAt: Date;
+   stripeId: string;
+   totalAmount: string;
+   event: {
+      _id: string;
+      title: string;
+   };
+   buyer: {
+      _id: string;
+      firstName: string;
+      lastName: string;
+   };
+}
+
+export type IOrderItem = {
+   _id: string;
+   totalAmount: string;
+   createdAt: Date;
+   eventTitle: string;
+   eventId: string;
+   buyer: string;
+};
+
+const OrderSchema = new Schema(
+   {
+      stripeId: {
+         type: String,
+         required: true,
+         unique: true,
+      },
+      totalAmount: {
+         type: String,
+      },
+      event: {
+         type: Schema.Types.ObjectId,
+         ref: "Event",
+      },
+      buyer: {
+         type: Schema.Types.ObjectId,
+         ref: "User",
+      },
+   },
+   { timestamps: true }
+);
+
+const Order = models.Order || model("Order", OrderSchema);
+
+export default Order;
