beca6334cd5e5cb36026777776f0c57fb2311954
Abdullah-142 <abdulah14200@gmail.com>
2024-04-01 09:40:18 -0700

    Remove unused import in FileUploader component

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useCallback, Dispatch, SetStateAction } from "react";
-// import type { FileWithPath } from "@uploadthing/react";
 import { useDropzone } from "@uploadthing/react/hooks";
 import { generateClientDropzoneAccept } from "uploadthing/client";
 
