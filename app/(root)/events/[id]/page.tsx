72d680035be51ec228953d3005b703b99cf4e53d
Abdullah-142 <abdulah14200@gmail.com>
2024-04-04 05:45:09 -0700

    Update text formatting in EventDetailsPage component

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -73,7 +73,9 @@ const EventDetailsPage = async ({ params: { id } }: SearchParamProps) => {
                </div>
 
                <div className="flex flex-col gap-2">
-                  <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
+                  <p className="p-bold-20 text-grey-600">
+                     What You&apos;ll Learn:
+                  </p>
                   <p className="p-medium-16 lg:p-regular-18">
                      {event.description}
                   </p>
