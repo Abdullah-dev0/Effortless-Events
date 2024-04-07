8b27b949612d9d94b520edd2880f46b999cfa672
Abdullah-142 <abdulah14200@gmail.com>
2024-04-07 04:36:33 -0700

    Add getRelatedEventsByCategory function to event.actions.ts

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -145,3 +145,34 @@ export async function updateEvent({ userId, event, path }: UpdateEventParams) {
       handleError(error);
    }
 }
+
+export async function getRelatedEventsByCategory({
+   categoryId,
+   eventId,
+   limit = 3,
+   page = 1,
+}: GetRelatedEventsByCategoryParams) {
+   try {
+      await connectToDatabase();
+
+      const skipAmount = (Number(page) - 1) * limit;
+      const conditions = {
+         $and: [{ category: categoryId }, { _id: { $ne: eventId } }],
+      };
+
+      const eventsQuery = Event.find(conditions)
+         .sort({ createdAt: "desc" })
+         .skip(skipAmount)
+         .limit(limit);
+
+      const events = await populateEvent(eventsQuery);
+      const eventsCount = await Event.countDocuments(conditions);
+
+      return {
+         data: JSON.parse(JSON.stringify(events)),
+         totalPages: Math.ceil(eventsCount / limit),
+      };
+   } catch (error) {
+      handleError(error);
+   }
+}
