de77fba1647100240efe5ce19ece9d355021d741
Abdullah-142 <abdulah14200@gmail.com>
2024-04-07 15:03:42 -0700

    Update webhook routes in middleware.ts and clerk route.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-   publicRoutes: ["/", "/events/:id", "/api/webhooks(.*)", "/api/uploadthing"],
+   publicRoutes: [
+      "/",
+      "/events/:id",
+      "/api/webhooks/clerk",
+      "/api/webhooks/stripe",
+      "/api/uploadthing",
+   ],
 });
 
 export const config = {
